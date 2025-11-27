a498246d78bfeb4be660fca65d781ddf371da531
ritikkr <ritikkumar2405199@gmail.com>
2025-11-28 04:10:19 +0530

    changes

--- README.md
+++ README.md
@@ -7,3 +7,20 @@ https://www.youtube.com/redirect?event=video_description&redir_token=QUFFLUhqbEJ
 ![alt text](image-1.png)
 
 ![alt text](image-2.png)
+
+![alt text](image-3.png)
+
+![alt text](image-4.png)
+
+![alt text](image-5.png)
+
+![alt text](image-6.png)
+
+![alt text](image-7.png)
+
+![alt text](image-8.png)
+
+![alt text](<Screenshot 2025-11-28 at 4.00.46 AM.png>)
+
+Reinforcement Learning with human feedback (RLHF)
+![alt text](image-9.png)
--- fastapi/demo.py
+++ fastapi/demo.py
@@ -2,7 +2,27 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+"""
+Upgrade pip and install FastAPI plus a server (uvicorn):
+python3 -m pip install --upgrade pip
+pip install fastapi uvicorn[standard]
+Run your app with uvicorn (replace demo:app with your module:app if different):
+python3 -m uvicorn demo:app --reload
 
+https://fastapi.tiangolo.com/tutorial/path-params/
+
+http://127.0.0.1:8000/docs
+
+"""
+itemList = [{"id":1, "name": "Item1"}]
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/items/{item_id}")
+async def getItems(item_id):
+    for item in itemList:
+        if item["id"] == item_id:
+            return item
+    
+    return {"no item found"}
